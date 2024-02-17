79eb69dc58aba7017e804928d2448e26229649dd
arkoleini <arkoleini@yahoo.com>
2024-02-17 10:52:37 -0600

    latest update

--- course-project-cmp-final/course-project-cmp-final/src/app/header/header.component.ts
+++ course-project-cmp-final/course-project-cmp-final/src/app/header/header.component.ts
@@ -1,9 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html'
 })
 export class HeaderComponent {
+  @Output('itemSelected') selectedItem = new EventEmitter<string>();
 
+
+  onSelectRecipes(){
+    this.selectedItem.emit('Recipes');
+  }
+
+  onSelectShoppinglist(){
+    this.selectedItem.emit('ShoppingList');
+  }
 }
