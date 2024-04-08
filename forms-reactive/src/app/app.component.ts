fecfceb71c8b81bf77ba44418a506e604603e75a
arkoleini <arkoleini@yahoo.com>
2024-04-07 22:39:53 -0500

    hook promise ValueChanged and StatusChanged

--- forms-reactive/src/app/app.component.ts
+++ forms-reactive/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -11,14 +12,23 @@ export class AppComponent implements OnInit {
     this.signupForm = new FormGroup({
       'userData': new FormGroup({
         username: new FormControl(null, [Validators.required,this.forbiddenNames.bind(this)]),
-        email: new FormControl('', [Validators.required, Validators.email]),
+        email: new FormControl('', [Validators.required, Validators.email], this.forbiddenEmails),
       }),
       gender: new FormControl('male'),
+    });
+
+    this.signupForm?.valueChanges.subscribe(data => {
+      console.log(`***valueChanges: ${JSON.stringify(data)}`);
+    });
+
+    this.signupForm?.statusChanges.subscribe(data => {
+      console.log(`***statusChanges: ${JSON.stringify(data)}`);
     });
   }
   signupForm: FormGroup;
   genders = ['male', 'female'];
   forbiddenUserNames = ['root', 'admin', 'user'];
+
 
   onSubmit() {
     console.log(this.signupForm.value);
@@ -32,4 +42,19 @@ export class AppComponent implements OnInit {
       return null;
     }
   }
+
+
+forbiddenEmails(control: FormControl): Promise<any> | Observable<any> {
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (control.value === 'test@test.com') {
+        resolve({ 'emailIsForbidden': true });
+      } else {
+        resolve(null);
+      }
+    }, 1500);
+  });
+  return promise;
+}
+
 }
